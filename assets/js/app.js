5d27e53705772aac1f1a4708bd2b921ce95323c2
Luteranya Nyange <dieume.dev@gmail.com>
2019-01-23 15:37:04 +0300

    refactoring: creation of DOMstrings object in the UI module to store all HTML classes used in the JS file

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,15 +4,24 @@ var budgetController = (function () {})();
 
 // UI Module
 var UIController = (function () {
-
+    // Stores all HTML Class
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    };
     return {
         // Get user Input
         getInput: function () {
             return {
-                type: document.querySelector('.add__type').value,
-                description: document.querySelector('.add__description').value,
-                value: document.querySelector('.add__value').value
+                type: document.querySelector(DOMstrings.inputType).value,
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
             }
+        },
+        getDOMstrings: function () {
+            return DOMstrings;
         }
     }
 })();
@@ -21,7 +30,8 @@ var UIController = (function () {
 // Data Module/ App Module
 var AppController = (function (bugdetCtrl, UICtrl) {
 
-    document.querySelector('.add__btn').addEventListener('click', function () {
+    var DOM = UICtrl.getDOMstrings();
+    document.querySelector(DOM.inputBtn).addEventListener('click', function () {
         // 1. Get field input Data
         var input = UICtrl.getInput();
         // 2. Add the item to the buget controller
