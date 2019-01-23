4df19a032980bb0151a135ca897d7716c03005fc
Luteranya Nyange <dieume.dev@gmail.com>
2019-01-23 15:22:41 +0300

    getting user input

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,8 +3,30 @@
 var budgetController = (function () {})();
 
 // UI Module
-var UIController = (function () {})();
+var UIController = (function () {
+
+    return {
+        // Get user Input
+        getInput: function () {
+            return {
+                type: document.querySelector('.add__type').value,
+                description: document.querySelector('.add__description').value,
+                value: document.querySelector('.add__value').value
+            }
+        }
+    }
+})();
 
 
 // Data Module/ App Module
-var AppController = (function (bugdetCtrl, UICtrl) {})(budgetController, UIController);
+var AppController = (function (bugdetCtrl, UICtrl) {
+
+    document.querySelector('.add__btn').addEventListener('click', function () {
+        // 1. Get field input Data
+        var input = UICtrl.getInput();
+        // 2. Add the item to the buget controller
+        // 3. Add the item to the UI
+        // 4. Calculate the budget
+        // 5. Display the budget on the UI
+    });
+})(budgetController, UIController);
