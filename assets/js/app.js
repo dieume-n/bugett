d23637f00acd78bfc99c4378a16ad0d515bb724a
Luteranya Nyange <dieume.dev@gmail.com>
2019-01-23 14:35:48 +0300

    implementation of separation of concern design principle by creation modules for each component of the application

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+// @ts-check
+// Controller Module
+var budgetController = (function () {})();
+
+// UI Module
+var UIController = (function () {})();
+
+
+// Data Module/ App Module
+var AppController = (function (bugdetController, UIController) {})(budgetController, UIController);
