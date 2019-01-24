83d1ea84e5b8601088402cdaf5b89543b8f029ac
Luteranya Nyange <dieume.dev@gmail.com>
2019-01-24 16:27:47 +0300

    input validation i.e no empty values nor expense or income with value of 0

--- assets/js/app.js
+++ assets/js/app.js
@@ -80,7 +80,7 @@ var UIController = (function () {
             return {
                 type: document.querySelector(DOMstrings.inputType).value,
                 description: document.querySelector(DOMstrings.inputDescription).value,
-                value: document.querySelector(DOMstrings.inputValue).value
+                value: parseFloat(document.querySelector(DOMstrings.inputValue).value)
             }
         },
         addListItem: function (obj, type) {
@@ -152,20 +152,33 @@ var AppController = (function (bugdetCtrl, UICtrl) {
         });
     };
 
+    var updateBudget = function () {
+        // 1. Calculate the budget
+        // 2. Return the budget
+        // 3. Display the budget on the UI
+    };
+
+
     var addItem = function () {
         var input, newItem;
 
         // 1. Get field input Data
         input = UICtrl.getInput();
 
-        // 2. Add the item to the buget controller
-        newItem = bugdetCtrl.addItem(input.type, input.description, input.value);
+        // Form validation
+        if (input.description !== '' && !isNaN(input.value) && input.value > 0) {
+            // 2. Add the item to the buget controller
+            newItem = bugdetCtrl.addItem(input.type, input.description, input.value);
 
-        // 3. Add the item to the UI
-        UICtrl.addListItem(newItem, input.type);
-        // 4. Clear the fields
-        UICtrl.clearFields();
-        // 5. Display the budget on the UI
+            // 3. Add the item to the UI
+            UICtrl.addListItem(newItem, input.type);
+
+            // 4. Clear the fields
+            UICtrl.clearFields();
+
+            // 5. Calculate and update budget
+            updateBudget();
+        }
     };
 
     return {
